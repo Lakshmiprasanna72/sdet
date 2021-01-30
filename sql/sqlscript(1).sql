fa6aeeafe103eb3ae3acb0ecdfc366ce8461679a
Lakshmiprasanna72 <76985961+Lakshmiprasanna72@users.noreply.github.com>
2021-01-30 23:10:33 +0530

    Add files via upload

---
+++ sql/sqlscript(1).sql
@@ -0,0 +1,13 @@
+REM   Script: Activity1
+REM   SQL
+
+create table salesman(salesman_id int,salesman_name varchar2(10),salesman_city varchar2(20),commission int);
+
+desc salesman
+
+
+create table salesman1(salesman_id int,salesman_name varchar2(10),salesman_city varchar2(20),commission int);
+
+desc salesman1
+
+
